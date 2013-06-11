5468e14647bf6ef52542e8126e0b1a21253bfb86
Michelle Madden <maddenmichelle@yahoo.com>
2013-06-10 20:29:44 -0400

    Sort games in chronological order closes #1

--- server.rb
+++ server.rb
@@ -4,11 +4,14 @@ Bundler.require
 require 'sinatra/reloader' unless ENV['RACK_ENV'] == 'production'
 require_relative "game"
 
-TEAMS = [3263, 2450]
+TEAMS = [3263, 2450, 3581, 1545, 3277]
 
 
 get '/' do
   @games = Game.for_teams(TEAMS)
   @games.select! { |g| !g.played? }
+  @games.sort!
   erb :index
 end
+
+
