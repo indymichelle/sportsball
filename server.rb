369e0623b482c6552ea37bf73f21b46f796a0620
Michelle Madden <maddenmichelle@yahoo.com>
2014-06-02 19:39:05 -0400

    Added new summer teams

--- server.rb
+++ server.rb
@@ -4,7 +4,7 @@ Bundler.require
 require 'sinatra/reloader' unless ENV['RACK_ENV'] == 'production'
 require_relative "game"
 
-TEAMS = [3263, 2450, 3581, 1545, 3277, 51, 1265, 3131, 1814, 3060, 4056, 4057]
+TEAMS = [3263, 2450, 3581, 1545, 3277, 51, 1265, 3131, 1814, 3060, 4056, 4057, 3764, 1593]
 
 
 get '/' do
