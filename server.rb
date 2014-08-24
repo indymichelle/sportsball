7a7ca0c40247acafcc0e9030649612c369d04a6c
Michelle Madden <maddenmichelle@yahoo.com>
2014-08-24 17:49:50 -0400

    Added new team for fall football

--- server.rb
+++ server.rb
@@ -4,7 +4,7 @@ Bundler.require
 require 'sinatra/reloader' unless ENV['RACK_ENV'] == 'production'
 require_relative "game"
 
-TEAMS = [3263, 2450, 3581, 1545, 3277, 51, 1814, 1265, 3060, 3764, 1593, 4395]
+TEAMS = [3263, 2450, 3581, 1545, 3277, 51, 1814, 1265, 3060, 3764, 1593, 4395, 3123]
 
 
 get '/' do
